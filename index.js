758f19c481a9c6e31925ea96b5bca1ca0ac5ffe7
Samsara Harb <samsaraharbb@gmail.com>
2024-10-20 20:12:50 -0500

    se transformó en comentario todo el código de js que buscaba automatizar el  carrusel por no saber como aplicarlo de forma que funcionara correctamente. Se aceptan sugerencias

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let currentSlide = 0;
 const totalSlides = 3;
-const intervalTime = 3000;
-const slideContainer = document.getElementById('carousel-slide-container');
+/* const intervalTime = 3000;
+const slideContainer = document.querySelector('.carousel-slide-container');
 function updateSlidePosition(){
-  const slideWidth = 100;
+  const slideWidth = slideContainer.offsetWidth;
   currentSlide++;
   if (currentSlide >= totalSlides) {
     currentSlide =0;
   }
-  slideContainer.style.transform = `translateX(-${currentSlide * slideWidth}%)`;
+  slideContainer.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
 }
-setInterval(updateSlidePosition, intervalTime);
+setInterval(updateSlidePosition, intervalTime); */
