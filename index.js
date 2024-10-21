c930f86aaad36e83c04abc4a444e8413222d4677
Samsara Harb <samsaraharbb@gmail.com>
2024-10-20 23:45:15 -0500

    se comentó las partes de HTLM que pertenecian a las imagenes de slide1 y slide2

--- index.js
+++ index.js
@@ -11,3 +11,28 @@ function updateSlidePosition(){
   slideContainer.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
 }
 setInterval(updateSlidePosition, intervalTime); */
+const slideContainer = document.querySelector('.carousel-slide-container');
+const anteriorButton = document.getElementById('anterior');
+const siguienteButton = document.getElementById('siguiente');
+function updateSlidePosition(){
+  const slideWidth = slideContainer.offsetWidth;
+  currentSlide++;
+  if (currentSlide >= totalSlides) {
+    currentSlide =0;
+  }
+  slideContainer.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+}
+anteriorButton.addEventListener('click', function() {
+  currentSlide--;
+  if(currentSlide<0){
+    currentSlide = totalSlides -1;
+  }
+  updateSlidePosition()
+});
+siguienteButton.addEventListener('click', function() {
+  currentSlide++;
+  if(currentSlide >= totalSlides){
+    currentSlide = 0;
+  }
+  updateSlidePosition()
+});
