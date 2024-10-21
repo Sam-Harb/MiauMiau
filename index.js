632244fba2f0fc7aaecd389c23da0ea54a2a6fc2
Samsara Harb <samsaraharbb@gmail.com>
2024-10-21 00:41:51 -0500

    se logró agregar el mousover que agrande las imagenes

--- index.js
+++ index.js
@@ -10,13 +10,14 @@ function updateSlidePosition(){
   }
   slideContainer.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
 }
-setInterval(updateSlidePosition, intervalTime); */
+setInterval(updateSlidePosition, intervalTime);  */
 const slideContainer = document.querySelector('.carousel-slide-container');
 const anteriorButton = document.getElementById('anterior');
 const siguienteButton = document.getElementById('siguiente');
+const carouselSlide = document.querySelectorAll(".carousel-slide");
 function updateSlidePosition(){
   const slideWidth = slideContainer.offsetWidth;
-  // currentSlide++;
+  //  currentSlide+;
   if (currentSlide >= totalSlides) {
     currentSlide =0;
   }
@@ -35,4 +36,14 @@ siguienteButton.addEventListener('click', function() {
     currentSlide = 0;
   }
   updateSlidePosition()
+});
+carouselSlide.forEach(slide => {
+  slide.addEventListener('mouseover', function() {
+    this.style.transform = 'scale(1.2)';
+    this.style.transition = 'transform 0.5s ease';
+  });
+
+  slide.addEventListener('mouseout', function() {
+    this.style.transform = 'scale(1)';
+  });
 });
